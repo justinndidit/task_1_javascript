e0bd6e62cc6031eb5535e30929d98dd6afec6ded
justinndidit <favourolasoji@student.oauife.edu.ng>
2024-07-03 08:17:40 +0100

    Updated weather api link, needs to protect apikeys

--- src/handlers/appHandler.ts
+++ src/handlers/appHandler.ts
@@ -6,22 +6,32 @@ import axios from "axios";
 export async function getUserDetailsAndGreet(request: Request, response: Response) {
     const visitorName = request.query.visitor_name as string;
     const userIp  = request.socket.remoteAddress?.toString();
-    const ipInfoBaseUrl = process.env.ipAddressBaseUrl?.toString();
-    const ipInfoToken = process.env.ipAddressApiToken?.toString();
-    const weatherApiBaseUrl = process.env.weatherApiBaseUrl?.toString();
-    const weatherApiToken = process.env.weatherApiToken?.toString();
-    const ipInfoUrl: string = `https://${ipInfoBaseUrl}/${userIp}?token=${ipInfoToken}`;
+    // const ipInfoBaseUrl = process.env.ipAddressBaseUrl?.toString();
+    // const ipInfoToken = process.env.ipAddressApiToken?.toString();
+    // const weatherApiBaseUrl = process.env.weatherApiBaseUrl?.toString();
+    // const weatherApiToken = process.env.weatherApiToken?.toString();
+
+    //This is bad
+    const weatherApiBaseUrl="https://api.tomorrow.io/v4/weather/forecast";
+    const weatherApiToken="cz10yWbD2RPNm0h8V8qZeCR6bMbbTnvj";
+    const ipAddressBaseUrl="ipinfo.io/";
+    const ipAddressApiToken="2b6d7978deef20";
+
+
+    const ipInfoUrl: string = `https://${ipAddressBaseUrl}/${userIp}?token=${ipAddressApiToken}`;
 
     try {
     
         const ipInfoResponse = await axios.get(ipInfoUrl);
         const ipInfoData = ipInfoResponse.data;
 
-        // console.log(ipInfoData);
-
         let locationData = ipInfoData?.loc?.split(" ") || ["6.5244" ,"3.3792"];
+        const long = locationData[0] || "6.5244";
+        const lat = locationData[1] || "3.3792";
         const city = ipInfoData.city || "Lagos";
-        const weatherApiUrl: string = `${weatherApiBaseUrl}?location=${locationData[0]},${locationData[1]}&apikey=${weatherApiToken}`;
+
+        //'https://api.tomorrow.io/v4/weather/forecast?location=new%20york&apikey=cz10yWbD2RPNm0h8V8qZeCR6bMbbTnvj'
+        const weatherApiUrl: string = `${weatherApiBaseUrl}?location=${city}&apikey=${weatherApiToken}`;
         const weatherApiResponse = await axios.get(weatherApiUrl);
         // console.log(weatherApiResponse);
         const weatherData = weatherApiResponse.data.timelines.minutely[0].values.temperature;
